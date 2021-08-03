09182367ac53b3fed73a74f9ee76775418ee7dde
Danny <dp7520293@gmail.com>
2021-08-03 19:56:27 +0100

    git ignore fixed

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,7 +3,7 @@ import { useAuth } from "../auth/auth";
 import { history } from "../routes/AppRouter";
 
 import logo192 from "../images/logo192.png";
-import Container from "@material-ui/core/Container";
+
 import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
